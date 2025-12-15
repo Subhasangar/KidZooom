a6aa1e661b3eef38c069f5ec789034e98f7a6bcb
SUBHASANGAR <ssubhasangar@gmail.com>
2025-12-14 21:55:46 -0800

    remove vercel config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.jsx';
 console.log("main.jsx loaded");
 
-createRoot(document.getElementById('root')).render(
+ReactDOM,createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
