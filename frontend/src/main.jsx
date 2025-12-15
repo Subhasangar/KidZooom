33475fade484e1bae6005c2fbcba16db93e216c8
SUBHASANGAR <ssubhasangar@gmail.com>
2025-12-14 22:02:06 -0800

    fix react root mounting

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.jsx';
 console.log("main.jsx loaded");
 
-ReactDOM,createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
