e9c62deae19913f4ba1be6600e9c6c992f8bb7c3
SUBHASANGAR <ssubhasangar@gmail.com>
2025-12-16 05:19:39 -0800

    fix vercel .json in frontend 4

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,9 +7,9 @@ console.log("main.jsx loaded");
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+   
     <App />
-    </BrowserRouter>
+   
   </React.StrictMode>
 
 );
